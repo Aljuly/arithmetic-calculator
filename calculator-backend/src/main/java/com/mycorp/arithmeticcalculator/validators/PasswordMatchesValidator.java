fac2aee229ade08439793e5ea9c5dc5ae8a5cffa
Aljuly <alg252@yandex.ru>
2019-10-18 17:58:59 +0300

    auth wip

---
+++ calculator-backend/src/main/java/com/mycorp/arithmeticcalculator/validators/PasswordMatchesValidator.java
@@ -0,0 +1,19 @@
+package com.mycorp.arithmeticcalculator.validators;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.mycorp.arithmeticcalculator.dto.UserDto;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+	@Override
+	public void initialize(PasswordMatches constraintAnnotation) {
+	}
+
+	@Override
+	public boolean isValid(Object obj, ConstraintValidatorContext context) {
+		UserDto user = (UserDto) obj;
+		return user.getPassword().equals(user.getMatchingPassword());
+	}
+}
