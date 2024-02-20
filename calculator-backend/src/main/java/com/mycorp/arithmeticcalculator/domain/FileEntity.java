03de9d57a191cb0c38ea6583a411abf985b4cbb4
aljuly <alzhulinsky@gmail.com>
2024-02-20 22:19:56 +0200

    file controller debug

--- calculator-backend/src/main/java/com/mycorp/arithmeticcalculator/domain/FileEntity.java
+++ calculator-backend/src/main/java/com/mycorp/arithmeticcalculator/domain/FileEntity.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
@@ -13,6 +15,7 @@ import javax.persistence.Table;
 public class FileEntity {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	@Lob
